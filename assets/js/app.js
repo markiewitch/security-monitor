677fbbacdcbad9161f7f7527b32f01f1052d98f1
Michał Markiewicz <markiewicz.m@outlook.com>
2018-08-06 22:15:55 +0200

    use new css on project list

---
+++ assets/js/app.js
@@ -0,0 +1,20 @@
+/*
+ * Welcome to your app's main JavaScript file!
+ *
+ * We recommend including the built version of this JavaScript file
+ * (and its CSS file) in your base layout (base.html.twig).
+ */
+
+// any CSS you require will output into a single css file (app.css in this case)
+require('../css/app.css');
+
+require('../../node_modules/bootstrap/dist/js/npm');
+require('../../node_modules/bootstrap/dist/css/bootstrap.css');
+require('../../node_modules/admin-lte/dist/css/AdminLTE.css');
+require('../../node_modules/admin-lte/dist/css/skins/skin-black.css');
+require('../../node_modules/font-awesome/css/font-awesome.css');
+
+// Need jQuery? Install it with "yarn add jquery", then uncomment to require it.
+// var $ = require('jquery');
+
+console.log('Hello Webpack Encore! Edit me in assets/js/app.js');
