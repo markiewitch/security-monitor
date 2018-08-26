a3089161473da107f1956a49fd0565bc69419946
Michał Markiewicz <markiewicz.m@outlook.com>
2018-08-26 18:51:28 +0200

    render chart with real data fetched asynchronously

---
+++ src/Service/VulnerabilitiesChartDataProvider.php
@@ -0,0 +1,44 @@
+<?php
+declare(strict_types=1);
+
+namespace App\Service;
+
+use Doctrine\DBAL\Connection;
+use Ramsey\Uuid\UuidInterface;
+
+class VulnerabilitiesChartDataProvider
+{
+    private $connection;
+
+    public function __construct(Connection $connection)
+    {
+        $this->connection = $connection;
+    }
+
+    public function prepareData(UuidInterface $projectId)
+    {
+        $sql = "select MAX(vulnerabilities_count) as vulnerabilities, finished_at from checks c WHERE c.project_id = :project_id AND c.finished_at > DATE_SUB(NOW(), INTERVAL 31 DAY) GROUP BY DAY(c.finished_at)";
+
+        $checks = $this->connection->executeQuery(
+            $sql,
+            ['project_id' => $projectId],
+            ['project_id' => 'uuid_binary_ordered_time'])->fetchAll();
+
+        $data = [];
+        foreach ($this->hydrate($checks) as $day) {
+            $data[] = $day;
+        }
+
+        return $data;
+    }
+
+    private function hydrate(array $rows)
+    {
+        foreach ($rows as $row) {
+            yield [
+                't' => (new \DateTime($row['finished_at']))->format("Y-m-d"),
+                'y' => $row['vulnerabilities'],
+            ];
+        }
+    }
+}
