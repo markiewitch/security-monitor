8637971a97cdf47b0b06a214fda0bb67ba70fb34
Michał Markiewicz <mmarkiewicz@gog.com>
2018-09-11 23:31:09 +0200

    add knp paginator to project list (#8)

--- src/Repository/OrmProjectsRepository.php
+++ src/Repository/OrmProjectsRepository.php
@@ -6,19 +6,22 @@ namespace App\Repository;
 
 use App\Entity\Project;
 use Doctrine\ORM\EntityManagerInterface;
-use Ramsey\Uuid\Uuid;
+use Knp\Component\Pager\Pagination\PaginationInterface;
+use Knp\Component\Pager\PaginatorInterface;
 use Ramsey\Uuid\UuidInterface;
 
 class OrmProjectsRepository implements ProjectsRepository
 {
-    /**
-     * @var EntityManagerInterface
-     */
+    /** @var EntityManagerInterface */
     private $em;
 
-    public function __construct(EntityManagerInterface $em)
+    /** @var PaginatorInterface */
+    private $paginator;
+
+    public function __construct(EntityManagerInterface $em, PaginatorInterface $paginator)
     {
-        $this->em = $em;
+        $this->em        = $em;
+        $this->paginator = $paginator;
     }
 
     /**
@@ -26,17 +29,15 @@ class OrmProjectsRepository implements ProjectsRepository
      * @param int $after
      * @return Project[]
      */
-    public function fetchLatest(int $limit = 10, int $after = 0): array
+    public function fetchLatest(int $page = 1): PaginationInterface
     {
         $qb = $this->em->createQueryBuilder()
             ->select('p')
             ->from('App:Project', 'p')
             ->addOrderBy('p.lastCheckDate', 'desc')
-            ->addOrderBy('p.uuid')
-            ->setMaxResults($limit)
-            ->setFirstResult($after);
+            ->addOrderBy('p.uuid');
 
-        return $qb->getQuery()->getResult();
+        return $this->paginator->paginate($qb, $page);
     }
 
     public function persist(Project $project)
