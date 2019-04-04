ee372d0fba92926aa56c06f89f507d16200ffc2a
Michał Markiewicz <mmarkiewicz@gog.com>
2019-04-04 13:19:29 +0200

    fix old migrations

--- src/Migrations/Version20180128141012.php
+++ src/Migrations/Version20180128141012.php
@@ -2,7 +2,7 @@
 
 namespace DoctrineMigrations;
 
-use Doctrine\DBAL\Migrations\AbstractMigration;
+use Doctrine\Migrations\AbstractMigration;
 use Doctrine\DBAL\Schema\Schema;
 
 /**
@@ -10,7 +10,7 @@ use Doctrine\DBAL\Schema\Schema;
  */
 class Version20180128141012 extends AbstractMigration
 {
-    public function up(Schema $schema)
+    public function up(Schema $schema): void
     {
         // this up() migration is auto-generated, please modify it to your needs
         $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
@@ -18,7 +18,7 @@ class Version20180128141012 extends AbstractMigration
         $this->addSql('CREATE TABLE projects (uuid CHAR(36) NOT NULL COMMENT \'(DC2Type:uuid)\', organization VARCHAR(255) NOT NULL, name VARCHAR(255) NOT NULL, was_last_check_successful TINYINT(1) DEFAULT NULL, last_check_date DATETIME DEFAULT NULL, PRIMARY KEY(uuid)) DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci ENGINE = InnoDB');
     }
 
-    public function down(Schema $schema)
+    public function down(Schema $schema): void
     {
         // this down() migration is auto-generated, please modify it to your needs
         $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
